02f8f00204dd7540d89833257aad86882824e39f
rh-abir <rhabir.co@gmail.com>
2023-06-06 23:01:46 +0600

    first commit

---
+++ src/routers/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>THis is p </p>,
+  },
+]);
+
+export default router;
